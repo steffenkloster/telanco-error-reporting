f6a268be29aa20ccd66be78807287f894bcfb62d
Steffen <steffenkopa@gmail.com>
2021-11-18 01:45:23 +0100

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoose = require("mongoose");
+const TError = require ('./models/TError');
+
+mongoose
+	.connect(global.process.env.NODE_ENV === 'production' ? 'mongodb://root:mongo@srv-captain--mongo/telanco?authSource=admin' : 'mongodb://localhost:27017/telanco', { useNewUrlParser: true })
+	.then(() => {
+		const app = express();
+		app.set('view engine', 'ejs');
+		app.use(cookieParser('bonner'));
+		app.use(express.json());
+		app.use(express.urlencoded({ extended: true }));
+		app.use(session({
+			secret: 'bonner',
+			resave: false,
+  			saveUninitialized: true
+		}));
+
+		app.use(function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			next();
+		});
+
+		app.get('/', async function(req, res) {
+			res.status(404).json({error: 404});
+		});
+
+		app.get('/view-secret-url-1389920', async function(req, res) {
+			const errors = await TError.getAllErrors();
+			res.render('index', { req, errors });
+		});
+
+		app.post('/report', async function(req, res) {
+			if(!req.body.error) {
+				TError.newError(req, {
+					website: 'INTERNAL',
+					errorMessage: `Recieved a POST request without a error element in body.`
+				});
+
+				return res.status(400).json({});
+			}
+			
+			TError.newError(req, req.body.error);
+			res.json({});
+		});
+
+		app.listen(5000, async () => {
+			console.log("Server has started!");
+		});
+	});
---
+++ models/TError.js
@@ -0,0 +1,107 @@
+const mongoose = require("mongoose");
+
+const tErrorSchema = mongoose.Schema({
+	date: {
+        type: Date,
+        default: () => Date.now()
+    },
+
+    website: String,
+    tags: [String],
+    ip: String,
+
+    filename: String,
+    errorType: {
+        type: String,
+        default: 'ERROR'
+    },
+    errorMessage: String,
+    stackTrace: String
+});
+
+const TError = mongoose.model('TError', tErrorSchema);
+
+module.exports = {
+    async newError(req, err) {
+        err.ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        return await new TError(err).save();
+    },
+
+    async getAllErrors() {
+        return await TError.find({});
+    }
+
+    /*
+    async createUser() {
+        let alias;
+        while(!alias) {
+            const a = generateAlias();
+            if(!(await this.aliasExists(a))) {
+                alias = a;
+            }
+        }
+
+        console.log(`Creating ${alias}`);
+        return await new User({ alias }).save();
+    },
+
+    async getUser(_id) {
+        if(!mongoose.Types.ObjectId.isValid(_id)) {
+            return null;
+        }
+        
+        return await User.findById(_id);
+    },
+
+    async getDosage(_id) {
+        if(!mongoose.Types.ObjectId.isValid(_id)) {
+            return null;
+        }
+        
+        return await User.findOne({ 'rides.dosages._id' : _id });
+    },
+
+    async changeDosage(_id, dosage) {
+        return await User.updateMany({},
+            { $set: { 'rides.$[].dosages.$[d].dose': dosage } },
+            { arrayFilters: [{ 'd._id': _id }] });
+    },
+
+    async changeDosageTime(_id, time) {
+        return await User.updateMany({},
+            { $set: { 'rides.$[].dosages.$[d].time': time } },
+            { arrayFilters: [{ 'd._id': _id }] });
+    },
+
+    async aliasExists(alias) {
+        return await User.findOne({ alias: { $regex : new RegExp(alias, "i") } });
+    },
+
+    // not working
+    async getCurrentTrainRide(_id) {
+        return await User.aggregate([
+            { $project: { id: 1, rides: { $slice: [ "$rides", -1 ] } } },
+            { $match: { _id } }
+         ]);
+    },
+
+    async startNewTrainRide(_id) {
+        return await User.findOneAndUpdate(
+            { _id }, 
+            { $push: {
+                rides: {
+                    startTime: new Date().toISOString(),
+                    dosages: []
+                }
+            } }
+        );
+    },
+
+    async setAlias(_id, alias) {
+        return await User.findOneAndUpdate(
+            { _id },
+            { alias }
+        );
+    }
+    */
+};
